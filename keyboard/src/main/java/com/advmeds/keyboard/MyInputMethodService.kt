92edc59caf73aea40632f88563732c0699f6c6ca
kevin <kevin@advmeds.com>
2020-08-12 16:29:42 +0800

    Update

--- keyboard/src/main/java/com/advmeds/keyboard/MyInputMethodService.kt
+++ keyboard/src/main/java/com/advmeds/keyboard/MyInputMethodService.kt
@@ -1,36 +1,54 @@
 package com.advmeds.keyboard
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
 import android.media.AudioManager
+import android.os.Build
 import android.text.InputType
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import androidx.core.content.res.ResourcesCompat
 
 open class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     private lateinit var keyboardView: KeyboardView
     private lateinit var keyboard: Keyboard
 
-    private var mCurKeyboard: Int = 0
-    private var caps: Boolean = false
+    private var mCurKeyboard = InputType.TYPE_NULL
+    private var caps = false
 
+    @SuppressLint("InflateParams")
     override fun onCreateInputView(): View {
 
-        keyboardView = layoutInflater.inflate(R.layout.keyboard, null).let { it as KeyboardView }.also {
+        keyboardView =
+            layoutInflater.inflate(R.layout.keyboard, null).let { it as KeyboardView }.also {
 
-            it.setOnKeyboardActionListener(this)
-            it.isPreviewEnabled = false
-        }
+                it.setOnKeyboardActionListener(this)
+                it.isPreviewEnabled = false
+            }
 
         return keyboardView
     }
 
-    override fun onPress(primaryCode: Int) { keyboardView.isPreviewEnabled = false }
+    override fun onPress(primaryCode: Int) {
+        keyboardView.isPreviewEnabled = false
+    }
 
     override fun onRelease(primaryCode: Int) { }
+
+    private fun Resources.getDrawableBy(id: Int): Drawable? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ResourcesCompat.getDrawable(this, id, null)
+        } else {
+            @Suppress("DEPRECATION")
+            this.getDrawable(id)
+        }
+    }
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
 
@@ -53,16 +71,19 @@ open class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardA
             Keyboard.KEYCODE_SHIFT -> {
 
                 caps = !caps
-                keyboard.setShifted(caps)
+                keyboard.isShifted = caps
                 keyboardView.invalidateAllKeys()
-                keyboard.keys.asSequence().first { it.codes.first() == primaryCode }?.icon = resources.getDrawable(if (caps) R.drawable.ic_keyboard_capslock else R.drawable.ic_keyboard_caps)
+                keyboard.keys.asSequence().first { it.codes.first() == primaryCode }?.icon = resources.getDrawableBy(if (caps) R.drawable.ic_keyboard_capslock else R.drawable.ic_keyboard_caps)
             }
-            0 -> { }
+            resources.getInteger(R.integer.keycode_undefined) -> {
+            }
             else -> {
 
                 var code = primaryCode.toChar()
 
-                if (code.isLetter() && caps) { code = code.toUpperCase() }
+                if (code.isLetter() && caps) {
+                    code = code.toUpperCase()
+                }
 
                 ic.commitText(code.toString(), 1)
             }
@@ -74,10 +95,10 @@ open class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardA
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
         val effectType = when (primaryCode) {
-            Keyboard.KEYCODE_DELETE  ->  AudioManager.FX_KEYPRESS_DELETE
-            Keyboard.KEYCODE_CANCEL  ->  AudioManager.FX_KEYPRESS_RETURN
-            32                       ->  AudioManager.FX_KEYPRESS_SPACEBAR
-            else                     ->  AudioManager.FX_KEYPRESS_STANDARD
+            Keyboard.KEYCODE_DELETE -> AudioManager.FX_KEYPRESS_DELETE
+            Keyboard.KEYCODE_CANCEL -> AudioManager.FX_KEYPRESS_RETURN
+            resources.getInteger(R.integer.keycode_spacebar) -> AudioManager.FX_KEYPRESS_SPACEBAR
+            else -> AudioManager.FX_KEYPRESS_STANDARD
         }
 
         audioManager.playSoundEffect(effectType)
@@ -85,20 +106,27 @@ open class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardA
 
     override fun onStartInput(attribute: EditorInfo?, restarting: Boolean) {
         super.onStartInput(attribute, restarting)
-        mCurKeyboard = attribute?.inputType ?: 0
+        mCurKeyboard = attribute?.inputType ?: InputType.TYPE_NULL
     }
 
     override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
         super.onStartInputView(info, restarting)
 
         keyboard = when (mCurKeyboard) {
-            InputType.TYPE_CLASS_NUMBER,
-            InputType.TYPE_CLASS_PHONE                      ->  Keyboard(this, R.xml.key_number)
-            InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS,
-            InputType.TYPE_CLASS_TEXT                       ->  Keyboard(this, R.xml.key_id)
-            InputType.TYPE_TEXT_VARIATION_PASSWORD,
-            InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD  ->  Keyboard(this, R.xml.key_password)
-            else                                            ->  Keyboard(this, R.xml.key_qwerty)
+            InputType.TYPE_CLASS_NUMBER, InputType.TYPE_CLASS_PHONE -> Keyboard(
+                this,
+                R.xml.key_number
+            )
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS -> Keyboard(
+                this,
+                R.xml.key_id
+            )
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD,
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD -> Keyboard(
+                this,
+                R.xml.key_password
+            )
+            else -> Keyboard(this, R.xml.key_qwerty)
         }
 
         keyboardView.keyboard = keyboard
@@ -114,13 +142,13 @@ open class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardA
         super.onUpdateExtractingVisibility(ei)
     }
 
-    override fun onText(text: CharSequence?) { }
+    override fun onText(text: CharSequence?) {}
 
-    override fun swipeRight() { }
+    override fun swipeRight() {}
 
-    override fun swipeLeft() { }
+    override fun swipeLeft() {}
 
-    override fun swipeUp() { }
+    override fun swipeUp() {}
 
-    override fun swipeDown() { }
+    override fun swipeDown() {}
 }
